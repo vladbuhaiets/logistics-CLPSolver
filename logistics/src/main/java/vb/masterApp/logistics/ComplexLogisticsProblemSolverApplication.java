90204489a51f46844755accc3c66109e5c9beffe
vladbuhaiets <vbuhaiets@gmail.com>
2023-11-09 12:54:44 +0200

    initial commit

---
+++ logistics/src/main/java/vb/masterApp/logistics/ComplexLogisticsProblemSolverApplication.java
@@ -0,0 +1,13 @@
+package vb.masterApp.logistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplexLogisticsProblemSolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplexLogisticsProblemSolverApplication.class, args);
+	}
+
+}
---
+++ logistics/src/test/java/vb/masterApp/logistics/ComplexLogisticsProblemSolverApplicationTests.java
@@ -0,0 +1,13 @@
+package vb.masterApp.logistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplexLogisticsProblemSolverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
